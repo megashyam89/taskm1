d60fdbbd65d5c36d92a70f1c53361e4243840ad7
Megashyam89 <megashyam031@gmail.com>
2025-09-06 15:09:05 +0530

    second commit

--- src/app_task4.jsx
+++ src/app_task4.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function app_task4() {
   return (
    <div>
-      <h1>Embedding Expressions Example</h1>
+      <h1>Embedding Expressions Example 1</h1>
       <p>The result of 2 + 3 is: {2 + 3}</p>
     </div>
   )
